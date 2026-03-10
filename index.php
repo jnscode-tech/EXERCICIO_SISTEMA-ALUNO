0b7f592241a1fe07184a2a30b426c858eb22548b
“jnscode-tech” <juliana_jns_2010@hotmail.com>
2026-03-10 17:41:48 -0300

    Sistema de Cadastro de Aluno. Feito o index e o cadastrar, as demais pendentes.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Escolar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+
+    <nav class="container-menu">
+    <ul>
+        <li><a href="cadastrar.php">CADASTRAR</a></li>
+        <li><a href="buscar.php">BUSCAR</a></li>
+        <li><a href="listar.php">LISTAR</a></li>
+        <li><a href="media.php">MEDIA</a></li>
+        <li><a href="sair.php">SAIR</a></li>
+        
+    </ul>
+</nav>
+</div>
+
+<div class="container-area">
+
+
+    <p>Sistema de Cadastro de Alunos</p>
+    
+
+</div>
+<footer>
+    Realizado por: Camila Macedo | Juliana Nascimento dos Santos
+</footer>
+</body>
+</html>
---
+++ listar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar aluno</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+
+    <nav class="container-menu">
+    <ul>
+        <li><a href="index.php">INÍCIO</a></li>
+        <li><a href="cadastrar.php">CADASTRAR</a></li>
+         <li><a href="buscar.php">BUSCAR</a></li>
+        <li><a href="media.php">MEDIA</a></li>
+        <li><a href="sair.php">SAIR</a></li>
+        
+    </ul>
+</nav>
+</div>
+
+<div class="container-area">
+
+
+    <p>LISTAR</p>
+    
+
+</div>
+<footer class="footer-paginas">
+    Realizado por: Camila Macedo | Juliana Nascimento dos Santos
+</footer>
+</body>
+</html>
